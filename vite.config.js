3a9139e49093fa1d51cdcf1cdc791789eb8698a2
suwubh <subhankarsatpathy69@gmail.com>
2025-10-06 12:43:10 +0530

    Changed vite.config.js for production

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,20 @@ export default defineConfig({
     port: 3000
   },
   build: {
-    outDir: 'build'
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'bootstrap-vendor': ['react-bootstrap', 'bootstrap'],
+          'icons-vendor': ['react-icons'],
+          'pdf-vendor': ['react-pdf', 'pdfjs-dist'],
+          'particles-vendor': ['@tsparticles/react', '@tsparticles/engine', '@tsparticles/slim', 'react-tsparticles'],
+          'utils-vendor': ['axios', '@emailjs/browser', 'typewriter-effect', 'react-parallax-tilt', 'react-github-calendar', 'react-router-dom']
+        }
+      }
+    }
   },
   assetsInclude: ['**/*.pdf'],
   optimizeDeps: {
